3622443043ad3eff3a92111c20bf17e06b9f8208
Added 2016-05-12 (L.Ramos-Munoz@cranfield.ac.uk) <L.Ramos-Munoz@cranfield.ac.uk>
2016-08-11 13:41:59 +0100

    Added SetUpData to gitignore

--- code_1/run.sh
+++ code_1/run.sh
@@ -3,17 +3,17 @@ echo "Running from"
 pwd
 cd build/bin
 if [ $1 == "R" ] ; then
-    upcrun LBMSolver
+    upcrun -shared-heap 1000MB LBMSolver
 elif [ $1 == "P" ] ; then
-    upcrun -shared-heap 1024MB LBMSolver
+    upcrun -shared-heap 1000MB LBMSolver
 elif [ $1 == "I" ] ; then
-    upcrun -shared-heap 1024MB LBMSolver
+    upcrun -shared-heap 1000MB LBMSolver
 elif [ $1 == "T" ] ; then
-    upcrun -shared-heap 1024MB -trace LBMSolver
+    upcrun -shared-heap 1000MB -trace LBMSolver
 elif [ $1 == "D" ] ; then
-    upcrun -shared-heap 1024MB -backtrace  LBMSolver
+    upcrun -shared-heap 1000MB -backtrace  LBMSolver
 elif [ $1 == "GDB" ] ; then
-    upcrun -shared-heap 1024MB -freeze=0 -backtrace LBMSolver
+    upcrun -shared-heap 1000MB -freeze=0 -backtrace LBMSolver
 
 fi
 cd ../../
